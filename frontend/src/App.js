f90091f06170d6bc3756596e3e61f06d7100bb89
fodela <particleme30@gmail.com>
2022-07-22 23:42:03 +0000

    installed and configured tailwind

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "./components/Layout/Layout";
+
+function App() {
+	return (
+		<div>
+			<h1>Welcome to Effect</h1>
+			<Layout></Layout>
+		</div>
+	);
+}
+
+export default App;
---
+++ frontend/src/components/Layout/Layout.js
@@ -0,0 +1,9 @@
+const Layout = () => {
+	return (
+		<div>
+			<h3>This is the layout page</h3>
+		</div>
+	);
+};
+
+export default Layout;
