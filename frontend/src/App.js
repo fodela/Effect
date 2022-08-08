f337adb9a976e599f98ddf77f5e5e9020db25ca6
fodela <particleme30@gmail.com>
2022-08-08 19:23:12 +0000

    feat: add background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import Layout from "./components/Layout/Layout";
 function App() {
 	return (
 		<div>
-			<h1>Welcome to Effect</h1>
 			<Layout></Layout>
 		</div>
 	);
--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -1,7 +1,17 @@
-const Layout = () => {
+const Layout = ({ children }) => {
 	return (
-		<div>
-			<h3>This is the layout page</h3>
+		<div
+			className="h-screen relative
+		bg-[rgba(0,0,0,.3)]"
+		>
+			<img
+				src="
+				https://images.unsplash.com/photo-1434394354979-a235cd36269d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1151&q=80"
+				alt="background-img"
+				className="object-cover w-full h-full absolute mix-blend-overlay text-center"
+			/>
+			<p className="text-blue-500">is this working?</p>
+			{children}
 		</div>
 	);
 };
