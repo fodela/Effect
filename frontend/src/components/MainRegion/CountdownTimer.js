54c865fe849a8e063de6abfcc07e19bc5267196f
fodela <particleme30@gmail.com>
2022-08-13 23:11:35 +0000

    feat: add countdown timer | stuck again

--- frontend/src/components/MainRegion/CountdownTimer.js
+++ frontend/src/components/MainRegion/CountdownTimer.js
@@ -1,43 +1,53 @@
 import { useState, useEffect } from "react";
 
 const defaultRemainingTime = {
-	minutes: "00",
-	seconds: "00",
+	minutes: 1,
+	seconds: 0,
 };
 
 const CountdownTimer = ({ countdownTime }) => {
-	let time = countdownTime * 60;
+	// let time = countdownTime * 60;
 	// Add the state of timer variables
-	let [remainingTime, setRemainingTime] = useState(defaultRemainingTime);
+	// let [remainingTime, setRemainingTime] = useState(defaultRemainingTime);
 
 	// call the update remaining time function every second using useEffect
-	useEffect(() => {
-		// create as a constant (to enable clearing the interval) a 1 second interval that calls the function when the component is mounted for the first time. This will create an infinite loop though hence the need for a clear interval function.
-		const intervalId = setInterval(() => {
-			updateRemainingTime();
-		}, 1000);
+	// useEffect(() => {
+	// 	// create as a constant (to enable clearing the interval) a 1 second interval that calls the function when the component is mounted for the first time. This will create an infinite loop though hence the need for a clear interval function.
+	// 	// const intervalId = setTimeout(() => {
+	// 	// 	setRemainingTime(getRemainingTime());
+	// 	// }, 1000);
+	// 	setTimeout(() => {
+	// 		setRemainingTime(getRemainingTime());
+	// 	}, 1000);
+	// 	// clear the interval
+	// 	// return () => clearTimeout(intervalId);
+	// });
 
-		// clear the interval
-		return () => clearInterval(intervalId);
-	}, []);
-
-	const getRemainingTime = () => {
-		let minutes = Math.floor(time / 60);
-		remainingTime.minutes = minutes < 10 ? "0" + minutes : minutes;
-		let seconds = time % 60;
-		remainingTime.seconds = seconds < 10 ? "0" + seconds : seconds;
-		console.log(remainingTime.minutes + ":" + remainingTime.seconds);
-		time--;
-		return remainingTime;
-	};
+	// const getRemainingTime = () => {
+	// 	let minutes = Math.floor(time / 60);
+	// 	remainingTime.minutes = minutes < 10 ? "0" + minutes : minutes;
+	// 	let seconds = time % 60;
+	// 	remainingTime.seconds = seconds < 10 ? "0" + seconds : seconds;
+	// 	console.log(remainingTime.minutes + ":" + remainingTime.seconds);
+	// 	time--;
+	// 	return remainingTime;
+	// };
 	// Create a function that updates the remaining time
-	const updateRemainingTime = () => {
-		setRemainingTime(getRemainingTime);
-	};
+	const [remainingTime, setRemainingTime] = useState(defaultRemainingTime);
+
+	useEffect(() => {
+		setTimeout((count) => {
+			// setRemainingTime((remainingTime) => remainingTime.minutes + 1);
+		}, 1000);
+	});
+	console.log(remainingTime.minutes + 1);
 	return (
 		<div>
 			<p>
-				<span>remainingTime</span>
+				<span>{remainingTime.minutes}</span>
+				{/* <span>
+					{remainingTime.minutes} : {remainingTime.seconds}
+				</span> */}
 			</p>
 		</div>
 	);
--- frontend/src/components/MainRegion/MainTask.js
+++ frontend/src/components/MainRegion/MainTask.js
@@ -1,7 +1,7 @@
 const MainTask = () => {
 	return (
 		<div className="flex text-xl gap-2">
-			<input type="checkbox" checked name="taskStatus" id="" />
+			<input type="checkbox" name="taskStatus" id="" />
 			<p className="">Post update on twitter</p>
 		</div>
 	);
