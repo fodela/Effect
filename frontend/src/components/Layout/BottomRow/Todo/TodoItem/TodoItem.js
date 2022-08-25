7cc1926b0a909f4007c08037a64096db961a09a9
fodela <particleme30@gmail.com>
2022-08-25 21:41:28 +0000

    npm run build

--- frontend/src/components/Layout/BottomRow/Todo/TodoItem/TodoItem.js
+++ frontend/src/components/Layout/BottomRow/Todo/TodoItem/TodoItem.js
@@ -1,35 +1,4 @@
 const TodoItem = (props) => {
-	let todoClass;
-
-	// task = props.done ? (
-	// 	<Fragment>
-	// 		<input
-	// 			type="checkbox"
-	// 			name="taskDone"
-	// 			id="taskDone"
-	// 			checked
-	// 			className="m-2"
-	// 			onChange={props.changeTodoState}
-	// 		/>
-
-	// 		<span className="task-description grow my-1 line-through text-[#eee6]">
-	// 			{props.task}
-	// 		</span>
-	// 	</Fragment>
-	// ) : (
-	// 	<Fragment>
-	// 		<input
-	// 			type="checkbox"
-	// 			name="taskDone"
-	// 			id="taskDone"
-	// 			className="m-2"
-	// 			onChange={props.changeTodoState}
-	// 		/>
-
-	// 		<span className="task-description grow my-1">{props.task}</span>
-	// 	</Fragment>
-	// );
-
 	return (
 		<div className="flex items-start group justify-between">
 			<div>
--- frontend/src/components/Layout/TopRow/TopRow.js
+++ frontend/src/components/Layout/TopRow/TopRow.js
@@ -1,7 +1,7 @@
 const TopRow = () => {
 	return (
 		<div className="region-top flex justify-between  place-items-center">
-			<div className="logo h-12 w-12 bg-[#444] rounded-full p-1">
+			<div className="logo h-12 w-12 bg-[#444] rounded-lg p-1">
 				<img src="logo.png" alt="" />
 			</div>
 
