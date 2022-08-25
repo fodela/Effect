e7c54fc3b91be60f51646e1dbe27acac7e50e0ee
fodela <particleme30@gmail.com>
2022-08-25 15:25:29 +0000

    style: animate quotes

--- frontend/src/components/Layout/RegionCenter/QuoteRegion/QuoteRegion.js
+++ frontend/src/components/Layout/RegionCenter/QuoteRegion/QuoteRegion.js
@@ -3,13 +3,20 @@ const QuoteRegion = () => {
 	const quote = Quote.getQuote();
 	return (
 		<div
-			className=" grow quote text-center w-5/6 mx-auto bg-black items-center justify-center flex flex-col gap-2
-            
+			className=" grow quote text-center w-5/6 mx-auto  items-center justify-center flex flex-col gap-2
+        
         "
 		>
-			<p>"{quote.text}"</p>
-			<div>
-				<p>{quote.author}</p>
+			<div className="group">
+				<p>"{quote.text}"</p>
+				<div
+					className="text-sm text-white/50 -mt-4 
+                invisible
+                group-hover:visible group-hover:translate-y-4 transition ease-out duration-500 
+                "
+				>
+					<p>{quote.author}</p>
+				</div>
 			</div>
 		</div>
 	);
