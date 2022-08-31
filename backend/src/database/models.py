ec644f8f743ae343f98e86ded352103193146204
fodela <particleme30@gmail.com>
2022-08-31 06:29:41 +0000

    feat: add setup_db to models.py

---
+++ backend/src/database/models.py
@@ -0,0 +1,31 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+
+# GET USERNAME AND PASSWORD FROM LOCAL ENV. SEE env_example for more info
+load_dotenv()
+
+DATABASE_USERNAME = os.getenv("DATABASE_USERNAME")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+
+
+# Define database variables
+database_name = "effect_db"
+database_path = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{'localhost:5432'}/{database_name}"
+
+# instantiate the database
+db = SQLAlchemy()
+
+"""
+setup_db(app)
+    binds a flask application and a SQALchemy service
+"""
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
