bd5eb19decabe04a4b51921fe9a11b804b8717d5
fodela <particleme30@gmail.com>
2022-09-07 18:52:23 +0000

    test: add todo for categories endpoints

---
+++ backend/src/endpoints/categories.py
@@ -0,0 +1,30 @@
+from tarfile import BLKTYPE
+from flask import Blueprint
+
+category = Blueprint("category", __name__, url_prefix="/api/v1")
+
+
+# [] get categories
+@category.get("categories")
+def get_categories(self):
+    pass
+
+
+# [] post categories
+@category.post("categories")
+def post_categories(self):
+    pass
+
+# [] patch categories
+
+
+@category.patch("categories")
+def patch_categories(self):
+    pass
+
+# [] delete categories
+
+
+@category.delete("categories")
+def delete_categories(self):
+    pass
