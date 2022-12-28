9cc925c1ffee08171066580ce5f3aa0003c361e3
fodela <particleme30@gmail.com>
2022-12-28 17:13:15 +0000

    feat: add login logic and control

--- frontend/src/components/Auth/Login.js
+++ frontend/src/components/Auth/Login.js
@@ -1,13 +1,72 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import axios from "axios";
+import AuthContext from "../../context/AuthProvider";
 
 const Login = () => {
+  const emailRef = useRef();
+  const pwdRef = useRef();
+
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
+  const [errMsg, setErrMsg] = useState("");
 
-  return email ? (
-    <input type="email" placeholder="Enter your email" />
-  ) : (
-    <input type="password" placeholder="Enter your password" />
+  const { setAuth } = useContext(AuthContext);
+  // autofocus
+  useEffect(() => {
+    emailRef.current.focus();
+  }, []);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/v1/auth/login",
+        { email, password },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            withCredentials: true,
+          },
+        }
+      );
+      const data = res.data;
+      setAuth({ email, password, data });
+    } catch (error) {
+      console.log(error);
+      setErrMsg(error.errMsg);
+    }
+  };
+
+  return (
+    <div className="text-3xl">
+      {!email ? (
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            setEmail(emailRef.current.value);
+            emailRef.current.value = "";
+          }}
+        >
+          <input
+            className="outline-none border-b-2 bg-white text-gray-900"
+            type="email"
+            placeholder="Enter your email"
+            ref={emailRef}
+          />
+        </form>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <input
+            className="outline-none border-b-2 bg-white text-gray-900"
+            type="password"
+            placeholder="Enter your password"
+            ref={pwdRef}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </form>
+      )}
+    </div>
   );
 };
 
