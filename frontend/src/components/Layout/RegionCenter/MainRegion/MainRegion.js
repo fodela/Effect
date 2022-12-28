584d1ce1ae6701abd63bf443c2bacbc0fb5072d4
fodela <particleme30@gmail.com>
2022-12-28 18:15:33 +0000

    feat: add auth logic

--- frontend/src/components/Layout/RegionCenter/MainRegion/MainRegion.js
+++ frontend/src/components/Layout/RegionCenter/MainRegion/MainRegion.js
@@ -1,16 +1,41 @@
+import { useState } from "react";
 import CountdownTimer from "../../../../containers/CountdownTimer";
+import Login from "../../../Auth/Login";
+import Signup from "../../../Auth/Signup";
 import MainTask from "./MainTask";
 import Salutation from "./Salutation";
 
 const MainRegion = ({ children }) => {
+  const [me, setMe] = useState(null);
+  const [isRegistered, setIsRegistered] = useState(false);
   return (
     <div
       className="p-5 mx-auto text-center
         flex flex-col justify-between gap-5 "
     >
-      <CountdownTimer />
-      <Salutation />
-      <MainTask />
+      {me ? (
+        <>
+          <CountdownTimer />
+          <Salutation />
+          <MainTask />
+        </>
+      ) : isRegistered ? (
+        <>
+          <Login />
+          <div>
+            Don't have an account?
+            <button onClick={() => setIsRegistered(false)}>Signup</button>
+          </div>
+        </>
+      ) : (
+        <>
+          <Signup />
+          <div>
+            Already have an account?
+            <button onClick={() => setIsRegistered(true)}> Login</button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
