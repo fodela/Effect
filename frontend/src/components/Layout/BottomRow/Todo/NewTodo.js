a6944d28400e557c7c8f22b79115634fe9837f13
fodela <particleme30@gmail.com>
2023-01-08 20:26:40 +0000

    feat: adds add new task functionality

--- frontend/src/components/Layout/BottomRow/Todo/NewTodo.js
+++ frontend/src/components/Layout/BottomRow/Todo/NewTodo.js
@@ -1,14 +1,65 @@
+import { useRef, useState } from "react";
+import axios from "../../../../api/axios";
+import useAuth from "../../../../hooks/useAuth";
+import useRefreshToken from "../../../../hooks/useRefreshToken";
+import useTasks from "../../../../hooks/useTasks";
+
 const NewTodo = (props) => {
-	return (
-		<input
-			type="text"
-			name="newTodo"
-			id="newTodo"
-			placeholder="New Todo"
-			className="mx-auto  p-2 ml-4 bg-black/5 w   rounded-md border-none
+  const inputRef = useRef();
+  const [description, setDescription] = useState("");
+  const [errMsg, setErrMsg] = useState(null);
+  const { auth } = useAuth();
+  const { access_token } = auth;
+
+  const { setTasks } = useTasks();
+
+  const refresh = useRefreshToken();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const controller = new AbortController();
+
+    try {
+      const res = await axios.post(
+        "/tasks",
+        { description },
+        {
+          signal: controller.signal,
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${access_token}`,
+          },
+          withCredentials: true,
+        }
+      );
+      setTasks((prev) => ({ ...prev, allTasks: res.data?.tasks }));
+      inputRef.current.value = "";
+      setErrMsg(null);
+    } catch (error) {
+      if (!error.response) {
+        setErrMsg("Network Error!");
+      } else if (error.response.data?.msg === "Token has expired") {
+        console.log(" 👍 💯 👎");
+        refresh();
+        handleSubmit();
+      } else {
+        console.log(error);
+        setErrMsg(error.response?.request?.statusText);
+      }
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      {errMsg && <p className="text-red-300"> 🧧Add todo error: {errMsg}</p>}
+      <input
+        type="text"
+        ref={inputRef}
+        onChange={(event) => setDescription(event.target.value)}
+        placeholder="New Todo"
+        className="p-2 ml-4 bg-inherit  rounded-md outline-none
 			"
-			onKeyUp={(event) => props.addTask(event)}
-		/>
-	);
+      />
+    </form>
+  );
 };
 export default NewTodo;
