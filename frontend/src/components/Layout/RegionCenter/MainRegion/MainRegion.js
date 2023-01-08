8ab102537692e73427cbb1316d1bfaa13806fb4e
fodela <particleme30@gmail.com>
2023-01-08 20:25:36 +0000

    refactor: main region ui

--- frontend/src/components/Layout/RegionCenter/MainRegion/MainRegion.js
+++ frontend/src/components/Layout/RegionCenter/MainRegion/MainRegion.js
@@ -1,65 +1,19 @@
-import { useEffect, useState } from "react";
-import axios from "../../../../api/axios";
 import CountdownTimer from "../../../../containers/CountdownTimer";
 
-import useAuth from "../../../../hooks/useAuth";
-import Login from "../../../Auth/Login";
-import Signup from "../../../Auth/Signup";
 import MainTask from "./MainTask";
 import Salutation from "./Salutation";
 
-const MainRegion = ({ children }) => {
-  const [me, setMe] = useState(null);
-  const [isRegistered, setIsRegistered] = useState(true);
-  const { auth } = useAuth();
-  const { access_token, refresh_token } = auth;
-
-  useEffect(() => {
-    console.log(" 👍", access_token);
-    const getUser = async () => {
-      try {
-        if (access_token) {
-          const res = await axios.get(
-            "auth/me",
-
-            { headers: { Authorization: `Bearer ${access_token}` } }
-          );
-          setMe(res.data?.user);
-        }
-      } catch (error) {
-        console.log(" 👎", error);
-      }
-    };
-    getUser();
-  }, [access_token, refresh_token]);
+const MainRegion = () => {
   return (
     <div
       className="p-5 mx-auto text-center
         flex flex-col justify-between gap-5 "
     >
-      {me ? (
-        <>
-          <CountdownTimer />
-          <Salutation />
-          <MainTask />
-        </>
-      ) : isRegistered ? (
-        <>
-          <Login />
-          <div>
-            Don't have an account?
-            <button onClick={() => setIsRegistered(false)}>Signup</button>
-          </div>
-        </>
-      ) : (
-        <>
-          <Signup />
-          <div>
-            Already have an account?
-            <button onClick={() => setIsRegistered(true)}> Login</button>
-          </div>
-        </>
-      )}
+      <>
+        <CountdownTimer />
+        <Salutation />
+        <MainTask />
+      </>
     </div>
   );
 };
