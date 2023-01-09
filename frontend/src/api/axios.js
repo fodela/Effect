76d7c22e88669b19faa682726ab1afbc050c456b
fodela <particleme30@gmail.com>
2023-01-09 17:30:43 +0000

    refactor: changes the axios baseURL to production link

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = "https://effect-backend.onrender.com/api/v1";
 export default axios.create({ baseURL: BASE_URL });
 
 export const axiosPrivate = axios.create({
