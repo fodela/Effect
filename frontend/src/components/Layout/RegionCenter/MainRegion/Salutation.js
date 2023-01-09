c56f72b4965e01e2dfdeb990ec00062a10a4632b
fodela <particleme30@gmail.com>
2023-01-09 22:13:11 +0000

    feat: adds dynamic username functionality

--- frontend/src/components/Layout/RegionCenter/MainRegion/Salutation.js
+++ frontend/src/components/Layout/RegionCenter/MainRegion/Salutation.js
@@ -1,8 +1,13 @@
+import useUser from "../../../../hooks/useUser";
+
 const Salutation = () => {
+  const { user } = useUser();
+
+  console.log(" 🕵️‍♂️", user);
   return (
     <div className="self-center text-3xl relative">
       <div className="bgShadow" />
-      Good evening, Fodela.
+      Good evening, <span className="capitalize">{user.username}</span>.
     </div>
   );
 };
